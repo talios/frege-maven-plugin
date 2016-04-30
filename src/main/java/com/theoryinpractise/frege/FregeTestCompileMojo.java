c9989cfe1b614a5a15f5a966bec23cf81f646ada
Mark Derricutt <mark@talios.com>
2016-04-30 17:17:36 +1200

    reformat pom/source

--- src/main/java/com/theoryinpractise/frege/FregeTestCompileMojo.java
+++ src/main/java/com/theoryinpractise/frege/FregeTestCompileMojo.java
@@ -11,48 +11,53 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-@Mojo(name = "test-compile", defaultPhase = LifecyclePhase.TEST_COMPILE, requiresDependencyResolution = ResolutionScope.TEST)
+@Mojo(
+  name = "test-compile",
+  defaultPhase = LifecyclePhase.TEST_COMPILE,
+  requiresDependencyResolution = ResolutionScope.TEST
+)
 public class FregeTestCompileMojo extends AbstractFregeCompileMojo {
 
-    @Parameter(defaultValue = "src/test/frege")
-    protected File testSourceDirectory;
+  @Parameter(defaultValue = "src/test/frege")
+  protected File testSourceDirectory;
 
-    @Parameter(required = true, defaultValue = "${project.build.directory}/generated-test-sources")
-    protected File generatedTestSourcesDirectory;
+  @Parameter(required = true, defaultValue = "${project.build.directory}/generated-test-sources")
+  protected File generatedTestSourcesDirectory;
 
-    @Parameter(required = true, defaultValue = "${project.build.directory}/generated-test-sources/frege")
-    protected File testOutputDirectory;
+  @Parameter(
+    required = true,
+    defaultValue = "${project.build.directory}/generated-test-sources/frege"
+  )
+  protected File testOutputDirectory;
 
-    @Parameter(required = true, readonly = true, property = "project.testClasspathElements")
-    protected List<String> testClasspathElements;
+  @Parameter(required = true, readonly = true, property = "project.testClasspathElements")
+  protected List<String> testClasspathElements;
 
-    public File getOutputDirectory() {
-        return testOutputDirectory;
+  public File getOutputDirectory() {
+    return testOutputDirectory;
+  }
+
+  public List<String> getClassPathElements() {
+    return testClasspathElements;
+  }
+
+  @Override
+  public void execute() throws MojoExecutionException, MojoFailureException {
+    project.addTestCompileSourceRoot(testOutputDirectory.getAbsolutePath());
+    super.execute();
+  }
+
+  @Override
+  public List<File> getAllSourceDirectories() {
+    List<File> sourceDirectories = new ArrayList<>();
+    for (File file : generatedTestSourcesDirectory.listFiles()) {
+      if (file.isDirectory()) {
+        sourceDirectories.add(file);
+      }
     }
-
-    public List<String> getClassPathElements() {
-        return testClasspathElements;
+    if (testSourceDirectory.exists()) {
+      sourceDirectories.add(testSourceDirectory);
     }
-
-    @Override
-    public void execute() throws MojoExecutionException, MojoFailureException {
-        project.addTestCompileSourceRoot(testOutputDirectory.getAbsolutePath());
-        super.execute();
-    }
-
-    @Override
-    public List<File> getAllSourceDirectories() {
-        List<File> sourceDirectories = new ArrayList<>();
-        for (File file : generatedTestSourcesDirectory.listFiles()) {
-            if (file.isDirectory()) {
-                sourceDirectories.add(file);
-            }
-        }
-        if (testSourceDirectory.exists()) {
-            sourceDirectories.add(testSourceDirectory);
-        }
-        return sourceDirectories;
-    }
-
-
+    return sourceDirectories;
+  }
 }
